c66993b177aadb65f1326a4896bf5bf07604a7c6
Natalia <n.piotrowski@tum.de>
2023-11-15 10:37:50 +0100

    add interactable and ladder

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,18 +4,30 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 @onready var anim=get_node("AnimatedSprite2D")
+var interactable=false
+@export var climbing=false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _unhandled_input(event):
+	if event.is_action_pressed("interact") and interactable:
+		print("control arms")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not climbing and not is_on_floor():
 		velocity.y += gravity * delta
 		anim.play("jump")		
 		if velocity.y>500:
 			velocity.y=500
+	elif climbing:
+		velocity.y=0
+		if Input.is_action_pressed("up"):
+			velocity.y=-SPEED
+		elif Input.is_action_pressed("down"):
+			velocity.y=SPEED
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -35,3 +47,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("ArmStation"):
+		interactable=true
+
+
+func _on_area_2d_area_exited(area):
+	if interactable:
+		interactable=false
