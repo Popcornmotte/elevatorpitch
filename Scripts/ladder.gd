9b7f180c3cba8fe87ea2632b3a20d0f695acbfc4
Natalia <n.piotrowski@tum.de>
2023-11-20 21:14:30 +0100

    make ladder better

--- Scripts/ladder.gd
+++ Scripts/ladder.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
+var collided=false
+@export var playerBody:Node2D
+
+# make sure that the player has to keep up or down pressed to keep on climbing
+func _process(delta):
+	if collided and Input.is_action_pressed("up"):
+		playerBody.climbing=true
+	elif collided and Input.is_action_pressed("down"):
+		playerBody.climbing=true
+	else:
+		playerBody.climbing=false
+		
 
 func _on_body_entered(body):
-	if body.name=="player" and  body.climbing==false:
-			body.climbing=true
+	if body.name=="player" :
+		collided=true
+			
 
 func _on_body_exited(body):
-	if body.name=="player" and body.climbing:
-			body.climbing=false
+	if body.name=="player":
+		collided=false
