646584929f41a85676f04243d7eeb1229410fd43
Popcornmotte <popcornmotte@gmx.net>
2023-12-17 19:37:49 +0100

    rope stuff works

--- Scenes/World/testEnvironments/test_scene_sandbox.gd
+++ Scenes/World/testEnvironments/test_scene_sandbox.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
-const ROPE = preload("res://Scripts/rope.gd")
+const ROPE = preload("res://Scenes/Objects/Test/rope.tscn")
 var startPos : Vector2
 var endPos : Vector2
+var pinned = false
 
-func _input(event):
-	if event == InputEventMouseButton:
-		if event.is_pressed():
-			startPos = get_global_mouse_position()
-		elif event.is_released():
-			endPos = get_global_mouse_position()
-			var rope = ROPE.instantiate(startPos, endPos)
+func _process(delta):
+	if Input.is_action_just_pressed("RightClick"):
+		pinned = !pinned
+		$Label.text = "Pinned mode "+str(pinned)
+	
+	if Input.is_action_just_pressed("Grab"):
+		print("pressed!")
+		startPos = get_global_mouse_position()
+	elif Input.is_action_just_released("Grab"):
+		print("released!")
+		endPos = get_global_mouse_position()
+		var rope = ROPE.instantiate()
+		
+		rope.global_position = startPos
+		rope.position = startPos
+		add_child(rope)
+		rope.spawn(startPos,endPos, pinned)
--- Scripts/rope.gd
+++ Scripts/rope.gd
@@ -1,27 +1,43 @@
 extends Node2D
 
 const SEGMENT = preload("res://Scenes/Objects/ropeSegment.tscn")
+const END = preload("res://Scenes/Objects/ropeEnd.tscn")
 var segments = []
-var segmentSize = 16
+var segmentSize = 24
+var ropeMass = 2.0
 
 
-func spawn(start : Vector2, end : Vector2):
+func spawn(start : Vector2, end : Vector2, pinned = false):
 	var length = start.distance_to(end)
 	var direction = (end-start).normalized()
 	var segmentCount = round(length / segmentSize)
 	var angle = Vector2(0,-1).angle_to(direction)
+	$Label.text = str(segmentCount)+" segments"
 	
 	for i in segmentCount:
-		var segment = SEGMENT.instantiate()
+		var segment
+		if(pinned && i == segmentCount-1):
+			segment = END.instantiate()
+		else:
+			segment = SEGMENT.instantiate()
+			segment.mass = ropeMass/segmentCount
 		segments.append(segment)
-		add_child(segment)
-		segment.position = i * direction * segmentSize
+		segment.global_position = i * direction * segmentSize
 		segment.rotation = angle
+		add_child(segment)
 		if(i == 0):
 			var joint = segment.get_node("Joint")
-			joint.node_a = self
-			joint.node_b = segments
+			joint.node_a = self.get_path()
+			joint.node_b = segment.get_path()
 		else:
 			var joint = segment.get_node("Joint")
-			joint.node_a = segments[i-1]
-			joint.node_b = segments
+			joint.node_a = segments[i-1].get_path()
+			joint.node_b = segment.get_path()
+
+func _draw():
+	for i in range(1,segments.size()):
+		draw_line(segments[i-1].position,segments[i].position,Color.BROWN,4)
+	pass
+
+func _process(delta):
+	queue_redraw()
