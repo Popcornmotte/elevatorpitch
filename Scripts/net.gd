9db5e6ad2728d6921a245788f9131cccac900bbf
Popcornmotte <popcornmotte@gmx.net>
2024-01-02 15:23:29 +0100

    net done

--- Scripts/net.gd
+++ Scripts/net.gd
@@ -3,11 +3,18 @@ extends Node2D
 const ROPE = preload("res://Scenes/Objects/Test/rope.tscn")
 @export var texture : Texture2D
 @export var ropeColor = Color.BLACK
+@export var speed = 400
+@export var maxDistance = 600
+@onready var lineLeft = $LineLeft
+@onready var lineRight = $LineRight
 var ropeArr : Array
 var arrSize
 var netPolygon : Polygon2D
 var rightEnd
 var rope
+var extended = true
+var animating = false
+var deploying = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rope = ROPE.instantiate()
@@ -28,45 +35,74 @@ func _ready():
 	add_child(netPolygon)
 	print(str(netPolygon))
 	rightEnd = ropeArr[arrSize-1]
-	pass # Replace with function body.
+	
+	#quickly hide and retract:
+	hide()
+	setDeployment(false)
 
-#func _draw():
-#	#fix for drawing the last part of the rope line 
-#	#draw_line(ropeArr[arrSize-2].global_position - global_position,$AnchorRightFollower.global_position - global_position,ropeColor,4)
-#	
-#	for i in range(1,arrSize):
-#		#draw_line(ropeArr[i-1].global_position - global_position,ropeArr[i].global_position - global_position,ropeColor,4)
-#		pass
-#	#for i in range(1,segments.size()):
-#	#		draw_line(segments[i-1].position,segments[i].position,color,4)
-#	pass
+
+func setDeployment(deploy : bool):
+	animating = true
+	if deploy:
+		show()
+		deploying = true
+		$AnimationPlayer.play_backwards("retract")
+		extended = true
+	else:
+		deploying = false
+		extended = false
+		$AnimationPlayer.play("RESET")
+		$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#rope.global_position = $AnchorLeft.global_position
 	#print("updating")
-	if Input.is_action_pressed("right"):
-		$AnchorRight.global_position.x += 120 * delta
-		$AnchorLeft.global_position.x += 120 * delta
-		#rope.global_position = $AnchorLeft.global_position
-	if Input.is_action_pressed("left"):
-		$AnchorRight.global_position.x -= 120 * delta
-		$AnchorLeft.global_position.x -= 120 * delta
-		#rope.global_position = $AnchorLeft.global_position
 	
-	if Input.is_action_just_pressed("W"):
-		$AnimationPlayer.play("retract")
-	if Input.is_action_just_pressed("down"):
-		$AnimationPlayer.play_backwards("retract")
+	lineLeft.points[1] = $ArmLeft/UpperArm/LowerArm/Tip.global_position - global_position
+	lineLeft.points[0] = $AnchorLeftFollower.global_position - global_position
+	lineRight.points[1] = $ArmRight/UpperArm/LowerArm/Tip.global_position - global_position
+	lineRight.points[0] = $AnchorRightFollower.global_position - global_position
 	
-	#queue_redraw()
+	if !animating and get_parent().controlArms:
+		if extended:
+			if Input.is_action_pressed("right"):
+				if($AnchorRight.position.x < maxDistance):
+					$AnchorRight.global_position.x += speed * delta
+					$AnchorLeft.global_position.x += speed * delta
+				#rope.global_position = $AnchorLeft.global_position
+			if Input.is_action_pressed("left"):
+				if($AnchorLeft.position.x > -maxDistance):
+					$AnchorRight.global_position.x -= speed * delta
+					$AnchorLeft.global_position.x -= speed * delta
+				#rope.global_position = $AnchorLeft.global_position
+		if Input.is_action_just_pressed("Q"):
+			if extended:
+				#$AnimationPlayer.play("retract")
+				#extended = false
+				setDeployment(false)
+			else:
+				#$AnimationPlayer.play_backwards("retract")
+				#extended = true
+				setDeployment(true)
 	
 	if Input.is_action_just_pressed("Debug"):
-		print("Polygon:")
-		for i in netPolygon.polygon.size():
-			print(str(i)+": "+str(netPolygon.polygon[i]))
+		print("AnchorRight: "+str($AnchorRight.position))
+		print("AnchorLeft: "+str($AnchorLeft.position))
+		#print("Polygon:")
+		#for i in netPolygon.polygon.size():
+		#	print(str(i)+": "+str(netPolygon.polygon[i]))
 	
 	netPolygon.polygon[0]=$AnchorLeftFollower.global_position - global_position
 	for i in range(0,arrSize):
 		netPolygon.polygon[i+1]=ropeArr[i].global_position - global_position
 	pass
+
+func _on_animation_player_animation_finished(anim_name):
+	if !extended:
+		hide()
+	animating = false
+
+
+func _on_timer_timeout():
+	$AnimationPlayer.play("retract")
