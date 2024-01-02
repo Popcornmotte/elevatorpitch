5634fab70a39d76e828184a362a96f455dbb177c
Natalia <n.piotrowski@tum.de>
2024-01-02 18:59:27 +0100

    fix i guess

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -234,6 +234,8 @@ func _on_interaction_area_area_entered(area):
 
 
 func _on_interaction_area_area_exited(area):
+	if area.owner==null:
+		return
 	if area.owner==interactionObject:
 		if interactionObject.name=="EngineDropButton":
 			interactionObject.close()
