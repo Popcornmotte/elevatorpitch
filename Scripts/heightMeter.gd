caced3ec6721776a49fe8b0c0fb795d78f6486b6
Natalia <n.piotrowski@tum.de>
2024-01-09 16:31:57 +0100

    fix behavior of dropped objects, heightmeter

--- Scripts/heightMeter.gd
+++ Scripts/heightMeter.gd
@@ -4,8 +4,8 @@ extends Node2D
 var startingPos=Vector2(0,10)
 var endPos=Vector2(0,-10)
 @export var finishHeight=50#currently hardcoded
-@onready var heightScale=finishHeight/20
+@onready var heightScale=2.5
 
 func _process(delta):
-	var leverPos=Vector2(0,round(Global.height/heightScale+10))
+	var leverPos=Vector2(0,round(10-Global.height/heightScale))
 	lever.position=leverPos
