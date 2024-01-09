8561ef952f4c5b6dc09de533be385b7206bb811d
Natalia <n.piotrowski@tum.de>
2024-01-09 17:34:06 +0100

    add better height meter from lukas

--- Scripts/heightMeter.gd
+++ Scripts/heightMeter.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
 @onready var lever=$Lever
-var startingPos=Vector2(0,10)
-var endPos=Vector2(0,-10)
 @export var finishHeight=50#currently hardcoded
-@onready var heightScale=2.5
+@onready var heightScale:float=2.77777
 
 func _process(delta):
-	var leverPos=Vector2(0,round(10-Global.height/heightScale))
+	var leverPos=Vector2(0,round(7-Global.height/heightScale))
 	lever.position=leverPos
