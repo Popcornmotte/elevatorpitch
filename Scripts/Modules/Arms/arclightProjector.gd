1a5ebb94be821ad5786514c955a30da73678320c
Tec <lukasmoersler@gmail.com>
2024-01-09 21:13:16 +0100

    Make modules consume fuel or ammo

--- Scripts/Modules/Arms/arclightProjector.gd
+++ Scripts/Modules/Arms/arclightProjector.gd
@@ -1,18 +1,26 @@
 extends GenericArmModule
 
 const boltPrefab = preload("res://Scenes/Objects/Modules/Arms/arclightBolt.tscn")
+const failNoise = preload("res://Assets/Audio/sfx/modules/arcFail.wav")
 
 @export var chargeCurve : Curve
 var maxChargeTime = 2.0
 var chargeTime = 0.0
 
 func activate():
+	if Global.countItem(Item.TYPE.Ammo) == 0:
+		$FailParticles.set_emitting(true)
+		Audio.playSfxLocalized(failNoise, global_position)
+		return
+	
 	active = true
 	parent.claw.setGrabLock(true, false)
 	$Charge.visible = true
 	$Audio.play()
 
 func deactivate():
+	if !active:
+		return
 	active = false
 	parent.claw.setGrabLock(false, false)
 	$Charge.scale = Vector2(0.5,0.5)
@@ -23,6 +31,7 @@ func deactivate():
 	Global.level.add_child(bolt)
 	bolt.global_position = $Charge.global_position
 	bolt.charge = chargeTime * 100
+	Global.takeFromInventory(Item.TYPE.Ammo)
 	
 	chargeTime = 0.0
 
