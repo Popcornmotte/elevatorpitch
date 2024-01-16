d2b0914da0c23afa6b657dfe2065ec153854b160
Popcornmotte <popcornmotte@gmx.net>
2024-01-16 18:13:08 +0100

    rocketeer

--- Scripts/Enemies/bullet.gd
+++ Scripts/Enemies/bullet.gd
@@ -2,14 +2,20 @@ extends Area2D
 
 var sprite : Node2D
 var velocity = Vector2(0,0)
-var damage = 4
+@export var damage = 4
+@export var explodeOnImpact = false
+var Explosion
 var isProjectile = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if explodeOnImpact:
+		Explosion = load("res://Scenes/Objects/explosion.tscn")
 	sprite = get_node("AnimatedSprite2D")
 	pass # Replace with function body.
 
+func flipH(arg : bool):
+	$AnimatedSprite2D.flip_h = arg
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -33,8 +39,20 @@ func _on_body_entered(body):
 	if body.owner != null:
 		if body.owner.name == "Elevator":
 			#print("Bullet found its target")
-			body.owner.takeDamage(damage, Global.DMG.Piercing)
+			if explodeOnImpact:
+				var explosion =  Explosion.instantiate()
+				get_parent().add_child(explosion)
+				explosion.global_position = global_position
+				call_deferred("queue_free")
+			else:
+				body.owner.takeDamage(damage, Global.DMG.Piercing)
 	else:
 		if body.has_method("takeDamage"):
-			print("I hit this: "+str(body.name))
-			body.takeDamage(damage,Global.DMG.Piercing)
+			#print("I hit this: "+str(body.name))
+			if explodeOnImpact:
+				var explosion =  Explosion.instantiate()
+				get_parent().add_child(explosion)
+				explosion.global_position = global_position
+				call_deferred("queue_free")
+			else:
+				body.takeDamage(damage,Global.DMG.Piercing)
